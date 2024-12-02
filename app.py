ec09325d7730659a07ddaeba8d49901532487cdc
Tony Montes <santiago.montesb@gmail.com>
2024-12-01 22:27:16 -0600

    base update

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ with open("assets/style.css") as css:
     st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 st.image('./assets/header.png')
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-def display_message(role, content, avatar, dataframe=None, sql_query=None):
+def display_message(role, content, avatar, dataframe=None, sql_query=None, add_to_history=True):
+    message = {"role": role, "content": content}
+    if dataframe is not None: message["dataframe"] = dataframe
+    if sql_query is not None: message["sql_query"] = sql_query
+    if add_to_history: st.session_state.messages.append(message)
     with st.chat_message(role, avatar=avatar):
         st.markdown(content)
         if role == "bot":
@@ -27,6 +28,9 @@ def display_message(role, content, avatar, dataframe=None, sql_query=None):
                 with st.expander("Ver la consulta SQL relacionada"):
                     st.code(sql_query, language="sql", line_numbers=True)
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 for message in st.session_state.messages:
     display_message(
         message["role"],
@@ -34,20 +38,16 @@ for message in st.session_state.messages:
         './assets/user-logo.png' if message["role"] == "human" else './assets/bot-logo.png',
         message.get("dataframe"),
         message.get("sql_query"),
+        add_to_history=False
     )
 
 if prompt := st.chat_input("¡Déjame mostrar mi magia! ¿Cuál es tu pregunta?"):
-    st.session_state.messages.append({"role": "human", "content": prompt})
     display_message("human", prompt, './assets/user-logo.png')
 
     response, status = answer(prompt)
     if status == 0:
         df = pd.DataFrame({"column1": [1, 2, 3], "column2": [4, 5, 6]})
         code = "SELECT * FROM table_name\nWHERE column1 = 1"
-        st.session_state.messages.append(
-            {"role": "bot", "content": response, "dataframe": df, "sql_query": code}
-        )
         display_message("bot", response, './assets/bot-logo.png', dataframe=df, sql_query=code)
     else:
-        st.session_state.messages.append({"role": "bot", "content": response})
         display_message("bot", response, './assets/bot-logo.png')
